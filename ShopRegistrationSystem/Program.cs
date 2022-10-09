fcee1fe79bf89b88448bd1eb476b74ade2c43371
Enes <77552232+enesgurel@users.noreply.github.com>
2022-10-09 23:27:46 +0300

    Add project files.

---
+++ ShopRegistrationSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShopRegistrationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
